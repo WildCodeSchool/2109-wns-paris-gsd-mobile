b73c4333fc7cee2fa39579814322116ac6d3340b
Matildad De Souza <mtld.desouza@gmail.com>
2022-02-21 10:40:56 +0100

    add navigation tabs

--- App.js
+++ App.js
@@ -1,27 +1,44 @@
+import React from 'react';
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import LoginScreen from "./screens/LoginScreen";
 import TaskScreen from "./screens/TaskScreen";
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          
+          if (route.Name === 'Login') {
+            iconName = focused ? 'log-in' : 'log-in-outline';
+          } else if (route.name === 'Tasks') {
+            iconName = focused ? 'tasks' : 'tasks-outline';
+          } 
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "pink",
+        tabBarInactiveTintColor: "gray",
+      })}
+        >
+        <Tab.Screen
           name="Login"
           component={LoginScreen}
-          options={{ headerShown: false }}
+          // options={{ headerShown: false }}
         />
-        <Stack.Screen
+        <Tab.Screen
           name="Task"
           component={TaskScreen}
           // options={{headerShown: false}}
         />
-      </Stack.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
