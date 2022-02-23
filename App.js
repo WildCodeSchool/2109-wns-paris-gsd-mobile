25d3eec990c724641f85b1f3b2e3336468216280
mineyas <mineyaskha@gmail.com>
2022-02-23 16:52:24 +0100

    modified navigation

--- App.js
+++ App.js
@@ -1,65 +1,69 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import LoginScreen from "./screens/LoginScreen";
 import TasksScreen from "./screens/TasksScreen";
-import TaskDetailsScreen from "./screens/TaskDetailsScreen";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import NotificationsScreen from "./screens/NotificationsScreen";
+import TaskDetailsScreen from "./screens/TaskDetailsScreen";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const Stack = createNativeStackNavigator();
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen
-
 const Tab = createBottomTabNavigator();
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === "All Tasks") {
+          iconName = focused ? "file-tray-full" : "file-tray-full-outline";
+        } else if (route.name === "Notifications") {
+          iconName = focused ? "notifications" : "notifications-outline";
+        }
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: "#FF00FF",
+      tabBarInactiveTintColor: "gray",
+    })}
+    >
+      {/* <Tab.Screen name="Home" component={Home} /> */}
+      <Tab.Screen
+        name="All Tasks"
+        component={TasksScreen}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={NotificationsScreen}
+        options={{ headerShown: false }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-          
-          if (route.name === 'Login') {
-            iconName = focused ? 'log-in' : 'log-in-outline';
-          } else if (route.name === 'Tasks') {
-            iconName = focused ? 'file-tray-full' : 'file-tray-full-outline';
-          } else if (route.name === 'Notifications') {
-            iconName = focused ? 'notifications' : 'notifications-outline';
-          } 
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: "#FF00FF",
-        tabBarInactiveTintColor: "gray",
-      })}
-        >
-        <Tab.Screen
+      <Stack.Navigator>
+        <Stack.Screen
           name="Login"
           component={LoginScreen}
           options={{ headerShown: false }}
         />
-        <Tab.Screen
+        <Stack.Screen
           name="Tasks"
-          component={TasksScreen}
+          component={HomeTabs}
           options={{ headerShown: false }}
         />
-        {/* <Stack.Screen
+        <Stack.Screen
           name="Task Details"
           component={TaskDetailsScreen}
           options={{ headerShown: false }}
-          // options={{headerShown: false}}
-        /> */}
-        <Tab.Screen
-          name="Notifications"
-          component={NotificationsScreen}
-          options={{headerShown: false}}
         />
-      </Tab.Navigator>
-
-      {/* </Stack.Navigator> */}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
