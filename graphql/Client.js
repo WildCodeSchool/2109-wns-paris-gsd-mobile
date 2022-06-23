2c0413e2dab2ffc872af19c8747bbbf5ca8b5aa0
mineyas <mineyaskha@gmail.com>
2022-06-23 17:47:55 +0200

    modified client

--- graphql/Client.js
+++ graphql/Client.js
@@ -1,7 +1,46 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import * as SecureStore from "expo-secure-store";
 
-// Initialize Apollo Client
+const httpLink = createHttpLink({
+  uri: "http://localhost:8000/graphql",
+});
+
+async function getValueFor(key) {
+  console.log("token fetched");
+  let result = await SecureStore.getItemAsync(key);
+  if (result) {
+    return result;
+  } else {
+    return null;
+  }
+}
+
+const authLink = setContext(async (req, { headers }) => {
+  // console.log(headers);
+
+  const token = await getValueFor("token");
+
+  console.log(token);
+  // const token = AsyncStorage.getItem("token");
+  const option = {
+    headers: {
+      ...headers,
+      Cookie: `token=${token}`,
+      // authorization: token,
+    },
+  };
+  console.log(option.headers);
+  return option;
+});
+
 export const client = new ApolloClient({
-  uri: 'http://192.168.1.95:8000/graphql',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+  // link: httpLink,
 });
